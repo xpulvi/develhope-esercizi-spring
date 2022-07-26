547206bfedcdbc2481d9186da4e979303d38922e
xpulvi <pulvirenti.n.a@gmail.com>
2022-07-26 13:26:42 +0200

    addExercisApi02
    add Exercis Api 02
    
    remuve Api01 on Swagger01 projet

---
+++ Api02/src/test/java/develhope_exercises/Api02/Api02ApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope_exercises.Api02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
