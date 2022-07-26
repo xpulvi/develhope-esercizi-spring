81816b78a40eba58c86172d324ae74194bde6d69
xpulvi <pulvirenti.n.a@gmail.com>
2022-07-26 12:18:07 +0200

    addEsercizApi01

---
+++ Api01/src/main/java/develhope_exercises/Api01/Api01Application.java
@@ -0,0 +1,13 @@
+package develhope_exercises.Api01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api01Application.class, args);
+	}
+
+}
---
+++ Api01/src/test/java/develhope_exercises/Api01/Api01ApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope_exercises.Api01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
