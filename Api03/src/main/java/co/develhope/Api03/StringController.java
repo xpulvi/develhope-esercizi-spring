8feef76c81de39f17c0fe93c370b7ca157ba11f4
xpulvi <pulvirenti.n.a@gmail.com>
2022-07-28 10:43:52 +0200

    randomAdd

---
+++ Api03/src/main/java/co/develhope/Api03/StringController.java
@@ -0,0 +1,15 @@
+package co.develhope.Api03;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/api")
+public class StringController {
+    
+
+}
---
+++ Api03/src/test/java/co/develhope/Api03/Api03ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Api03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
