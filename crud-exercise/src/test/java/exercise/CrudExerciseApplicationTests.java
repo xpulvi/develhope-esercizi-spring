5e2672b825064368afed8784e844db3b5838c635
xpulvi <pulvirenti.n.a@gmail.com>
2022-08-01 16:31:04 +0200

    comitCrud

---
+++ crud-exercise/src/test/java/exercise/CrudExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
