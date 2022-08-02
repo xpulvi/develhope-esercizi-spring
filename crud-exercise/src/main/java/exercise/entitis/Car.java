6ce83f687db85b4bdd3c4d70d6a8608b7ef0215b
xpulvi <pulvirenti.n.a@gmail.com>
2022-08-02 10:38:30 +0200

    miniAddEzercizCrud

---
+++ crud-exercise/src/main/java/exercise/entitis/Car.java
@@ -0,0 +1,17 @@
+package exercise.entitis;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    
+}
