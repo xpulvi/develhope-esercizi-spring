19a061a8994da57e7b3b2f1e015ca3237a040230
xpulvi <pulvirenti.n.a@gmail.com>
2022-08-02 09:35:33 +0200

    api04End

--- Api04/src/main/java/co/develhope/Api04/controller/HeaderController.java
+++ Api04/src/main/java/co/develhope/Api04/controller/HeaderController.java
@@ -1,57 +1,25 @@
 package co.develhope.Api04.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.InetSocketAddress;
+
 
 @RestController
-@RequestMapping("/api04")
+@RequestMapping("/header")
 public class HeaderController {
 
-    @Autowired
-    private HttpServletRequest request;
+    @GetMapping
+    ResponseEntity <String> getUrlEndPort(@RequestHeader HttpHeaders headers){
 
-    @RequestMapping(value = "/echo/{input:.+}", method = RequestMethod.GET)
-    public ModelAndView getDomain(@PathVariable("input") String input) {
+        InetSocketAddress myHost = headers.getHost();
 
-        ModelAndView modelandView = new ModelAndView("result");
-
-        modelandView.addObject("user-agent", getUserAgent());
-        modelandView.addObject("headers", getHeadersInfo());
-
-        return modelandView;
-
+        String url = "l'hot is: " + myHost.getHostName() + " la porta e: " + myHost.getPort();
+        return new ResponseEntity<String>(url, HttpStatus.OK);
     }
 
-    //get user agent
-    private String getUserAgent() {
-        return request.getHeader("user-agent");
-    }
-
-    //get request headers
-    private Map<String, String> getHeadersInfo() {
-
-        Map<String, String> map = new HashMap<String, String>();
-
-        Enumeration headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String key = (String) headerNames.nextElement();
-            String value = request.getHeader(key);
-            map.put(key, value);
-        }
-
-        return map;
-    }
-
-    @DeleteMapping("/echo")
-    public String deliteRoute() {
-
-        return "ciao io sono il delit";
-    }
 
 }
