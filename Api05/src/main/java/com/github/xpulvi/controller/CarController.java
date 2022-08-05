6f8d269b98c4bb9257d22a05cdc854406ef26fdb
xpulvi <pulvirenti.n.a@gmail.com>
2022-08-06 01:28:13 +0200

    Api05End

--- Api05/src/main/java/com/github/xpulvi/controller/CarController.java
+++ Api05/src/main/java/com/github/xpulvi/controller/CarController.java
@@ -1,12 +1,29 @@
 package com.github.xpulvi.controller;
 
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.xpulvi.dto.CarDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
-@M("/cars")
+@RequestMapping("/cars")
 public class CarController {
 
+    //on a GET request returns a new Car as response | io ho capito di fare cosi:
+    @GetMapping(value = "/echo")
+    public String getNewCsr(@RequestParam(required = true) String id, @RequestParam(required = true) String modelName,
+                     @RequestParam(required = false) Double price){
 
+        return "new Car" ;
+    }
+
+    // on a POST request prints the Car body in console and returns an HTTP Created success message to the user
+    @PostMapping(value = "/echo")
+    public String postPrintCarBody(@Valid @RequestBody(required = true)CarDTO car){
+        System.out.println(car.toString());
+        // non ho caoito bene come usarlo sul return: new ResponseBody<String>(HttpStatus.OK) sul return
+        return "HTTP Created success";
+    }
 
 }
--- Api05/src/main/java/com/github/xpulvi/dto/CarDTO.java
+++ Api05/src/main/java/com/github/xpulvi/dto/CarDTO.java
@@ -3,12 +3,15 @@ package com.github.xpulvi.dto;
 import lombok.Data;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @ToString
 public class CarDTO {
 
+    @NotBlank(message = "mandatory")
     private String id;
-
+    @NotBlank(message = "mandatory")
     private String modelName;
 
     private double price;
