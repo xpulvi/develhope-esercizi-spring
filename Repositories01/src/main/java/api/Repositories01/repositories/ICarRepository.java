2da4d80866ef572ca4c1fb607f09521436f32586
xpulvi <pulvirenti.n.a@gmail.com>
2022-09-23 00:56:45 +0200

    Addminor

--- Repositories01/src/main/java/api/Repositories01/repositories/ICarRepository.java
+++ Repositories01/src/main/java/api/Repositories01/repositories/ICarRepository.java
@@ -2,6 +2,8 @@ package api.Repositories01.repositories;
 
 import api.Repositories01.entitis.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICarRepository extends JpaRepository<Car, Long> {
 }
