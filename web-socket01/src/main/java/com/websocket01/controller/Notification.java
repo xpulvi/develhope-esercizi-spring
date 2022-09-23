a1fb009b085f22285dcce50918ff3332540ad9d4
xpulvi <pulvirenti.n.a@gmail.com>
2022-09-23 20:12:10 +0200

    addWebsocket01

---
+++ web-socket01/src/main/java/com/websocket01/controller/Notification.java
@@ -0,0 +1,33 @@
+package com.websocket01.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class Notification {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @PostMapping("/broadcast-message")
+    public ResponseEntity sendNotificationToClients(@RequestBody SimpleMessageDTO message){
+        simpMessagingTemplate.convertAndSend("/topic/messages", message);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+
+    @MessageMapping("/hello") // /app/hello
+    @SendTo("/topic/messages")
+    public SimpleMessageDTO handleMessageFromWebSocket(MessageFromClientDTO message){
+        System.out.println("Arrived someting on /app/hello - " + message.toString());
+        return new SimpleMessageDTO("Message from " + message.getFrom() + " - " + message.getText());
+    }
+
+}
---
+++ web-socket01/src/main/java/com/websocket01/entities/MessageDto.java
@@ -0,0 +1,14 @@
+package com.websocket01.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+
+    private String type;
+    private String message;
+}
